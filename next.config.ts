d5126348ffd023f2205db0937bbbc542361b6beb
lecs-o <hohner.levente@gmail.com>
2024-11-11 15:05:46 +0100

    config mod3

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   output: "standalone",
   assetPrefix: '',
   basePath: '',
+  images: {
+    loader: "akamai",
+    path: "/",
+  },
 };
 
 export default nextConfig;
